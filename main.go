0ef2758f4180c26043b67b9138b3598bb339ab0d
facundoschillino <facundoschillino01@gmail.com>
2023-10-17 20:45:09 -0300

    .

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/handlers"
 	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/repositories"
 	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/services"
@@ -12,6 +14,39 @@ var (
 	router        *gin.Engine
 )
 
+func main() {
+	router = gin.Default()
+	//Iniciar objetos de handler
+	dependencies()
+	//Iniciar rutas
+	mappingRoutes()
+
+	log.Println("Iniciando el servidor...")
+	router.Run(":8080")
+}
+func mappingRoutes() {
+	//middleware para permitir peticiones del mismo server localhost
+
+	//cliente para api externa
+	//var authClient clients.AuthClientInterface
+	//authClient = clients.NewAuthClient()
+	//creacion de middleware de autenticacion
+	//authMiddleware := middlewares.NewAuthMiddleware(authClient)
+
+	//Listado de rutas
+	groupCamion := router.Group("/trucks")
+	//Uso del middleware para todas las rutas del grupo
+	//group.Use(authMiddleware.ValidateToken)
+
+	//group.Use(middlewares.CORSMiddleware())
+
+	groupCamion.GET("/", camionHandler.ObtenerCamiones)
+	//group.GET("/:id", aulaHandler.ObtenerAulaPorID)
+	groupCamion.POST("/", camionHandler.InsertarCamion)
+	groupCamion.PUT("/:id", camionHandler.ModificarCamion)
+	groupCamion.DELETE("/:id", camionHandler.EliminarCamion)
+
+}
 func dependencies() {
 	var database repositories.DB
 	var camionRepo repositories.CamionRepositoryInterface
