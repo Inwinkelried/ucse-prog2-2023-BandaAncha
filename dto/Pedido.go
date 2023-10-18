2411702a35772beb620b8e63911e3928e61d849e
facundoschillino <facundoschillino01@gmail.com>
2023-10-18 20:40:06 -0300

    .

--- dto/Pedido.go
+++ dto/Pedido.go
@@ -1,11 +1,38 @@
 package dto
 
-import "time"
+import (
+	"time"
+
+	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/model"
+	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/utils"
+)
 
 type Pedido struct {
 	ID                string
-	Productos         []Producto
-	Usuario           Usuario
+	Productos         []PedidoProducto
+	Destino           string
+	Estado            string
 	FechaCreacion     time.Time
 	FechaModificacion time.Time
+}
+
+func NewPedido(pedido model.Pedido) *Pedido {
+	return &Pedido{
+		ID:                utils.GetStringIDFromObjectID(pedido.ID),
+		Productos:         []PedidoProducto{},
+		Destino:           pedido.Destino,
+		Estado:            "Pendiente",
+		FechaCreacion:     time.Now(),
+		FechaModificacion: time.Now(),
+	}
+}
+func (pedido Pedido) GetModel() model.Pedido {
+	return model.Pedido{
+		ID:                utils.GetObjectIDFromStringID(pedido.ID),
+		Productos:         []model.PedidoProducto{},
+		Destino:           pedido.Destino,
+		Estado:            pedido.Estado,
+		FechaCreacion:     pedido.FechaCreacion,
+		FechaModificacion: pedido.FechaModificacion,
+	}
 }
--- model/Pedido.go
+++ model/Pedido.go
@@ -1,1 +1,17 @@
 package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Pedido struct {
+	ID                primitive.ObjectID `bson:"_id,omitempty"`
+	Productos         []PedidoProducto   `bson:"productos"`
+	Cantidad          int                `bson:"cantidad"`
+	FechaCreacion     time.Time          `bson:"fechaCreacion,omitempty"`
+	FechaModificacion time.Time          `bson:"fechaModificacion,omitempty"`
+	Estado            string             `bson:"estado"`
+	Destino           string             `bson:"destino"`
+}
