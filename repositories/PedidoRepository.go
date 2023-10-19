ab2b192f130cd1ae7eea6bcb6aea4d4450800aa3
facundoschillino <facundoschillino01@gmail.com>
2023-10-19 11:13:23 -0300

    Pedidos avanzado
    Falta terminar pedidos handler y aplicar funciones en main

--- repositories/PedidoRepository.go
+++ repositories/PedidoRepository.go
@@ -1,1 +1,64 @@
 package repositories
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type PedidoRepositoryInterface interface {
+	ObtenerPedidos() ([]model.Pedido, error)
+	EliminarPedido(id primitive.ObjectID) (*mongo.DeleteResult, error)
+	InsertarPedido(pedido model.Pedido) (*mongo.InsertOneResult, error)
+	ModificarPedido(pedido model.Pedido) (*mongo.UpdateResult, error)
+}
+type PedidoRepository struct {
+	db DB
+}
+
+func NewPedidoRepository(db DB) *PedidoRepository {
+	return &PedidoRepository{
+		db: db,
+	}
+}
+func (repo PedidoRepository) ObtenerPedidos() ([]model.Pedido, error) {
+	lista := repo.db.GetClient().Database("BandaAncha").Collection("Pedidos")
+	filtro := bson.M{}
+
+	cursor, err := lista.Find(context.TODO(), filtro)
+
+	defer cursor.Close(context.Background())
+
+	var Pedidos []model.Pedido
+	for cursor.Next(context.Background()) {
+		var Pedido model.Pedido
+		err := cursor.Decode(&Pedido)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
+		Pedidos = append(Pedidos, Pedido)
+	}
+	return Pedidos, err
+}
+func (repo PedidoRepository) InsertarPedido(pedido model.Pedido) (*mongo.InsertOneResult, error) {
+	lista := repo.db.GetClient().Database("BandaAncha").Collection("Pedidos")
+	resultado, err := lista.InsertOne(context.TODO(), pedido)
+	return resultado, err
+}
+func (repo PedidoRepository) ModificarPedido(pedido model.Pedido) (*mongo.UpdateResult, error) {
+	lista := repo.db.GetClient().Database("BandaAncha").Collection("Pedidos")
+	filtro := bson.M{"_id": pedido.ID}
+	entity := bson.M{"$set": bson.M{"estado": pedido.Estado}}
+	resultado, err := lista.UpdateOne(context.TODO(), filtro, entity)
+	return resultado, err
+}
+func (repo PedidoRepository) EliminarPedido(id primitive.ObjectID) (*mongo.DeleteResult, error) {
+	lista := repo.db.GetClient().Database("BandaAncha").Collection("Pedidos")
+	filtro := bson.M{"_id": id}
+	resultado, err := lista.DeleteOne(context.TODO(), filtro)
+	return resultado, err
+}
--- services/PedidoService.go
+++ services/PedidoService.go
@@ -1,1 +1,45 @@
 package services
+
+import (
+	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/dto"
+	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/repositories"
+	"github.com/Inwinkelried/ucse-prog2-2023-BandaAncha/utils"
+)
+
+type PedidoServiceInterface interface {
+	ObtenerPedidos() []*dto.Pedido
+	InstertarPedido(pedido *dto.Pedido) bool
+	EliminarPedido(id string) bool
+	ModificarPedido(pedido *dto.Pedido) bool
+}
+
+type PedidoService struct {
+	pedidoRepository repositories.PedidoRepositoryInterface
+}
+
+func NewPedidoService(pedidoRepository repositories.PedidoRepositoryInterface) *PedidoService {
+	return &PedidoService{
+		pedidoRepository: pedidoRepository,
+	}
+}
+func (service PedidoService) ObtenerPedidos() *[]dto.Pedido {
+	pedidosDB, _ := service.pedidoRepository.ObtenerPedidos()
+	var pedidos []*dto.Pedido
+	for _, pedidodDB := range pedidosDB {
+		pedido := dto.NewPedido(pedidodDB)
+		pedidos = append(pedidos, pedido)
+	}
+	return service.ObtenerPedidos()
+}
+func (service PedidoService) InsertarPedido(pedido *dto.Pedido) bool {
+	service.pedidoRepository.InsertarPedido(pedido.GetModel())
+	return true
+}
+func (service PedidoService) ModificarPedido(pedido *dto.Pedido) bool {
+	service.pedidoRepository.ModificarPedido(pedido.GetModel())
+	return true
+}
+func (service PedidoService) EliminarPedido(id string) bool {
+	service.pedidoRepository.EliminarPedido(utils.GetObjectIDFromStringID(id))
+	return true
+}
