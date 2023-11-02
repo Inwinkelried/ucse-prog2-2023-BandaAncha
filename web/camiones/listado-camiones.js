6810309624d37aa435279a23e9d4d2c6f2462a65
Santiago Inwinkelried <101613134+Inwinkelried@users.noreply.github.com>
2023-11-01 23:48:11 -0300

    add: request.js y update: listado-camiones.js
    aun no trae los registros en el listado de camiones

--- web/camiones/listado-camiones.js
+++ web/camiones/listado-camiones.js
@@ -1,95 +1,47 @@
-// const url = `http://localhost:8082/trucks`;
-// const customHeaders = new Headers();
-// customHeaders.append("User-Agent", "PostmanRuntime/7.33.0");
-// customHeaders.append("Accept", "*/*");
-// customHeaders.append("Accept-Encoding", "gzip, deflate, br");
-// customHeaders.append("Connection", "keep-alive");
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     obtenerCamiones();
-// });
-
-// function obtenerCamiones() {
-//     const elementosTable = document
-//         .getElementById("elementosTable")
-//         .querySelector("tbody");
-
-//     fetch(url, {
-//         method: "GET",
-//         headers: customHeaders,
-//         redirect: "follow",
-//     })
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error("Error en la solicitud al servidor.");
-//             }
-//             return response.json();
-//         })
-//         .then((data) => {
-//             data.forEach((camion) => {
-//                 const row = document.createElement("tr"); //crear una fila
-//                 row.innerHTML = `
-//                         <td>${camion.id}</td>
-//                         <td>${camion.patente}</td>
-//                         <td>${camion.peso_maximo}</td>
-//                         <td>${camion.costo_km}</td>
-//                         <td>${camion.fecha_creacion}</td>
-//                         <td>${camion.fecha_modificacion}</td>
-//                     `;
-//                 elementosTable.appendChild(row);
-//             });
-//         })
-//         .catch((error) => {
-//             console.log("error", error);
-//             alert("error");
-//         });
-// }
-const url = `http://localhost:8082/trucks`;
-const customHeaders = new Headers();
-customHeaders.append("User-Agent", "PostmanRuntime/7.33.0");
-customHeaders.append("Accept", "*/*");
-customHeaders.append("Accept-Encoding", "gzip, deflate, br");
-customHeaders.append("Connection", "keep-alive");
-customHeaders.append("Accept", "*/*");
-customHeaders.append("Accept-Encoding", "gzip, deflate, br");
-customHeaders.append("Connection", "keep-alive");
-
 document.addEventListener("DOMContentLoaded", function (event) {
+    // if (!isUserLogged()) {
+    //   window.location.href =
+    //     window.location.origin + "/login.html?reason=login_required";
+    // }
+
     obtenerCamiones();
 });
 
 function obtenerCamiones() {
-    const elementosTable = document
+    urlConFiltro = `http://localhost:8080/trucks`; //ver que url colocariamos
+    makeRequest(
+        `${urlConFiltro}`,
+        Method.GET,
+        null,
+        ContentType.JSON,
+        CallType.PRIVATE,
+        exitoObtenerCamiones,
+        errorObtenerCamiones
+    );
+}
+
+function exitoObtenerCamiones(data) {
+    const elementosTable = document //tabla en la que se colocan los camiones que se obtienen
         .getElementById("elementosTable")
         .querySelector("tbody");
 
-    fetch(url, {
-        method: "GET",
-        headers: customHeaders,
-        redirect: "follow",
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            data.forEach((camion) => {
-                const row = document.createElement("tr"); //crear una fila
-                row.innerHTML = `
-                        <td>${camion.id}</td>
-                        <td>${camion.patente}</td>
-                        <td>${camion.peso_maximo}</td>
-                        <td>${camion.costo_km}</td>
-                        <td>${camion.fecha_creacion}</td>
-                        <td>${camion.fecha_modificacion}</td>
-                    `;
-                elementosTable.appendChild(row);
-            });
-        })
-        .catch((error) => {
-            console.error("Error en la solicitud al servidor:", error);
-            alert(`Error en la solicitud al servidor: ${error.message}`);
-        });
+    data.forEach((elemento) => {
+        const row = document.createElement("tr"); //crear una fila
+
+        row.innerHTML = ` 
+                <td>${elemento.Patente}</td>
+                <td>${elemento.PesoMaximo}</td>
+                <td>${elemento.CostoPorKilometro}</td>
+                <td>${elemento.FechaCreacion}</td>
+                <td>${elemento.FechaModificacion}</td>
+                <td class="acciones"><a href="form.html?patente=${elemento.patente}&tipo=EDITAR">Editar</a> | <a href="form.html?patente=${elemento.patente}&tipo=ELIMINAR">Eliminar</a></td>
+            `;
+        elementosTable.appendChild(row);
+    });
+}
+
+function errorObtenerCamiones(response) {
+    alert("Error en la solicitud al servidor.");
+    console.log(response.json());
+    throw new Error("Error en la solicitud al servidor.");
 }
