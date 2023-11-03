72903369aa92626b627a744f02ce4ad708b6c789
Santiago Inwinkelried <101613134+Inwinkelried@users.noreply.github.com>
2023-11-02 23:14:24 -0300

    feat: login, register, add: request.js, scripts.js

--- web/camiones/listado-camiones.js
+++ web/camiones/listado-camiones.js
@@ -1,70 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    cargarDatos();
+document.addEventListener("DOMContentLoaded", function (event) {
+    // if (!isUserLogged()) {
+    //     window.location.href =
+    //         window.location.origin + "/login.html?reason=login_required";
+    // }
+
+    obtenerCamiones();
 });
 
-function cargarDatos() {
-    fetch("/trucks", { method: "GET" })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Error al obtener datos de camiones.");
-            }
-            return response.json();
-        })
-        .then(data => {
-            mostrarDatosTabla(data);
-        })
-        .catch(error => {
-            console.error("Error al obtener datos de camiones:", error);
-        });
-};
+function obtenerCamiones() {
+    const urlConFiltro = `http://localhost:8080/trucks`; //ver que url colocariamos
+    makeRequest(
+        `${urlConFiltro}`,
+        Method.GET,
+        null,
+        ContentType.JSON,
+        CallType.PUBLIC, //ESTÁ EN PUBLIC PARA PROBAR. PONERLO EN PRIVADO
+        exitoObtenerCamiones,
+        errorObtenerCamiones
+    );
+}
 
-function mostrarDatosTabla(datos) {
-    var table = document.getElementById("elementosTable");
-    var tbody = document.getElementById("bodyTable");
+function exitoObtenerCamiones(data) {
+    const elementosTable = document //tabla en la que se colocan los camiones que se obtienen
+        .getElementById("elementosTable")
+        .querySelector("tbody");
 
-    function mostrarError() {
-        var row = document.createElement("tr");
-        var celdaError = document.createElement("td");
-        celdaError.colSpan = 6;
-        celdaError.textContent = "Error al obtener datos de camiones.";
-        row.appendChild(celdaError);
-        tbody.appendChild(row);
-    }
+    data.forEach((elemento) => {
 
-    datos.forEach(function (element) {
-        var row = document.createElement("tr");
+        const row = document.createElement("tr"); //crear una fila
 
-        var celdaId = document.createElement("td");
-        celdaId.textContent = element.ID;
-        celdaId.className = "nombreCelda";
-        row.appendChild(celdaId);
+        row.innerHTML = ` 
+                            <td>${elemento.patente}</td>
+                            <td>${elemento.peso_maximo}</td>
+                            <td>${elemento.costo_km}</td>
+                            <td>${elemento.fecha_creacion}</td>
+                            <td>${elemento.fecha_modificacion}</td>
+                            <td class="acciones"><a href="form.html?patente=${elemento.patente}&tipo=EDITAR">Editar</a> | <a href="form.html?patente=${elemento.patente}&tipo=ELIMINAR">Eliminar</a></td>
+                    `;
 
-        var celdaPatente = document.createElement("td");
-        celdaPatente.textContent = element.Patente;
-        row.appendChild(celdaPatente);
-
-        var celdaPeso = document.createElement("td");
-        celdaPeso.textContent = element.PesoMaximo;
-        row.appendChild(celdaPeso);
-
-        var celdaCosto = document.createElement("td");
-        celdaCosto.textContent = element.CostoKm;
-        row.appendChild(celdaCosto);
-
-        var celdaEditar = document.createElement("td");
-        var botonEditar = document.createElement("button");
-        botonEditar.className = "boton-editar";
-        botonEditar.innerHTML = `<i class="fa-solid fa-pen" style="color: #ffffff;"></i>`;
-        celdaEditar.appendChild(botonEditar);
-        row.appendChild(celdaEditar);
-
-        var celdaEliminar = document.createElement("td");
-        var botonEliminar = document.createElement("button");
-        botonEliminar.className = "boton-eliminar";
-        botonEliminar.innerHTML = `<i class="fa-solid fa-trash" style="color: #ffffff;"></i>`;
-        celdaEliminar.appendChild(botonEliminar);
-        row.appendChild(celdaEliminar);
-
-        tbody.appendChild(row);
+        elementosTable.appendChild(row);
     });
+}
+function errorObtenerCamiones(response) {
+    alert("Error en la solicitud al servidor.");
+    console.log(response.json());
+    throw new Error("Error en la solicitud al servidor.");
 }
